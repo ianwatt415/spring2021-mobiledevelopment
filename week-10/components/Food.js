954ecf64768e6a3569440092674c6e2f995ef3f4
ianwatt415 <77796743+ianwatt415@users.noreply.github.com>
2021-03-19 16:10:41 -0400

    Add files via upload

---
+++ week-10/components/Food.js
@@ -0,0 +1,12 @@
+import {Component} from "React";
+import {Text, View} from "react-native";
+
+export default class Food extends Component {
+    render() {
+        return (
+            <View>
+                <Text>Pizza</Text>
+            </View>
+        );
+    }
+}
---
+++ week-10/components/Name.js
@@ -0,0 +1,12 @@
+import {Component} from "React";
+import {Text, View} from "react-native";
+
+export default class Name extends Component {
+    render() {
+        return (
+            <View>
+                <Text>Ian Watt</Text>
+            </View>
+        );
+    }
+}
